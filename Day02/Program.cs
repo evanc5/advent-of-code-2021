1acd39b19268d60209d16194f46872f394f61284
Evan C <evanc5@users.noreply.github.com>
2021-12-01 10:18:22 -0500

    configure day 2

---
+++ Day02/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Day02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            var input = File.ReadAllLines(@".\input.txt");
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+
+            sw.Stop();
+            System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
+        }
+
+        static void Part2()
+        {
+            var input = File.ReadAllLines(@".\input.txt");
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+
+            sw.Stop();
+            System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+        }
+    }
+}
