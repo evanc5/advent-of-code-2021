4580c7c1f1cb6239a2bc3e68acec771d930648d4
Evan C <evanc5@users.noreply.github.com>
2021-12-03 01:32:15 -0500

    partial complete part 2 (not right)

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -57,33 +57,35 @@ namespace Day03
             var input = File.ReadAllLines(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
-            string gammaBinary = "";
-            string epsilonBinary = "";
             var lineLength = input[0].Length;
             var bitCounts = new int[lineLength];
-            foreach (var line in input)
-            {
-                for (int i = 0; i < lineLength; i++)
-                {
-                    if (line[i] == '1') bitCounts[i]++;
-                }
-            }
 
-            foreach (var count in bitCounts)
+            var o2List = input.ToArray();
+            var co2List = input.ToArray();
+            for (int bit = 0; bit < lineLength; bit++)
             {
-                if (count > input.Length / 2)
+                var highCount = 0;
+                foreach (var line in input)
                 {
-                    gammaBinary += '1';
-                    epsilonBinary += '0';
+                    if (line[bit] == '1') highCount++;
+                }
+                if (highCount >= input.Length / 2)
+                {
+                    if (o2List.Length > 1) o2List = o2List.Where(line => line[bit] == '1').ToArray();
+                    if (co2List.Length > 1) co2List = co2List.Where(line => line[bit] == '0').ToArray();
                 }
                 else
                 {
-                    gammaBinary += '0';
-                    epsilonBinary += '1';
+                    if (o2List.Length > 1) o2List = o2List.Where(line => line[bit] == '0').ToArray();
+                    if (co2List.Length > 1) co2List = co2List.Where(line => line[bit] == '1').ToArray();
                 }
             }
 
+            var o2Rating = Convert.ToInt32(o2List.First(), 2);
+            var co2Rating = Convert.ToInt32(co2List.First(), 2);
+
             sw.Stop();
+            Console.WriteLine($"Part 2: {o2Rating * co2Rating}");
             System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
         }
     }
