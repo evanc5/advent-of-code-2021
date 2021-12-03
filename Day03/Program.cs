d0b9f87843f70385c054bc4b3068da9d369d9044
Evan C <evanc5@users.noreply.github.com>
2021-12-03 00:33:30 -0500

    finish day 3 part 1, start part 2

--- Day03/Program.cs
+++ Day03/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Day03
 {
@@ -16,8 +18,37 @@ namespace Day03
             var input = File.ReadAllLines(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
+            string gammaBinary = "";
+            string epsilonBinary = "";
+            var lineLength = input[0].Length;
+            var bitCounts = new int[lineLength];
+            foreach (var line in input)
+            {
+                for (int i = 0; i < lineLength; i++)
+                {
+                    if (line[i] == '1') bitCounts[i]++;
+                }
+            }
+
+            foreach (var count in bitCounts)
+            {
+                if (count > input.Length / 2)
+                {
+                    gammaBinary += '1';
+                    epsilonBinary += '0';
+                }
+                else
+                {
+                    gammaBinary += '0';
+                    epsilonBinary += '1';
+                }
+            }
+
+            var gamma = Convert.ToInt32(gammaBinary, 2);
+            var epsilon = Convert.ToInt32(epsilonBinary, 2);
 
             sw.Stop();
+            Console.WriteLine($"Part 1: {gamma * epsilon}");
             System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
         }
 
@@ -26,6 +57,31 @@ namespace Day03
             var input = File.ReadAllLines(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
+            string gammaBinary = "";
+            string epsilonBinary = "";
+            var lineLength = input[0].Length;
+            var bitCounts = new int[lineLength];
+            foreach (var line in input)
+            {
+                for (int i = 0; i < lineLength; i++)
+                {
+                    if (line[i] == '1') bitCounts[i]++;
+                }
+            }
+
+            foreach (var count in bitCounts)
+            {
+                if (count > input.Length / 2)
+                {
+                    gammaBinary += '1';
+                    epsilonBinary += '0';
+                }
+                else
+                {
+                    gammaBinary += '0';
+                    epsilonBinary += '1';
+                }
+            }
 
             sw.Stop();
             System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
