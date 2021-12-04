32ac05e276adbd47f59782fb5fd1794c4002eed5
Evan C <evanc5@users.noreply.github.com>
2021-12-04 00:26:57 -0500

    start day 4

--- Day04/Program.cs
+++ Day04/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day04
 {
@@ -16,6 +18,8 @@ namespace Day04
             var input = File.ReadAllLines(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
+            var bingoCalls = input[0].Split(',');
+
 
             sw.Stop();
             System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
@@ -29,6 +33,53 @@ namespace Day04
 
             sw.Stop();
             System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+        }
+    }
+
+    class BingoBoard
+    {
+        public int[,] Board { get; private set; }
+        public bool[,] CalledBoard { get; private set; }
+
+        public BingoBoard(string[] input)
+        {
+            Board = new int[5, 5];
+            for (int i = 0; i < 5; i++)
+            {
+                var split = input[i].Split(' ').Where(n => !string.IsNullOrWhiteSpace(n)).Select(n => int.Parse(n)).ToArray();
+                for (int j = 0; j < 5; j++)
+                {
+                    Board[i, j] = split[j];
+                }
+            }
+        }
+
+        public bool Call(int n)
+        {
+            var indices = IndeciesOf(n);
+            foreach (var index in indices)
+            {
+                CalledBoard[index.Item1, index.Item2] = true;
+            }
+            return HasBingo();
+        }
+
+        public bool HasBingo()
+        {
+
+        }
+
+        private Tuple<int, int>[] IndeciesOf(int n)
+        {
+            var indecies = new List<Tuple<int, int>>();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 5; j++)
+                {
+                    if (Board[i, j] == n) indecies.Add(new(i, j));
+                }
+            }
+            return indecies.ToArray();
         }
     }
 }
