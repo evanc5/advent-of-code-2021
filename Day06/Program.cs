c4bf110916a3c1a977a6bf9ff1cc96264b0d4db3
Evan C <evanc5@users.noreply.github.com>
2021-12-06 18:27:31 -0500

    complete part 2 and refactor part 1

--- Day06/Program.cs
+++ Day06/Program.cs
@@ -18,27 +18,41 @@ namespace Day06
             var input = File.ReadAllText(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
-            var fishList = input.Split(',').Select(int.Parse).ToList();
+            var rawFishList = input.Split(',').Select(int.Parse);
+            var fishCounts = new Dictionary<int, int>()
+            {
+                {0, 0},
+                {1, 0},
+                {2, 0},
+                {3, 0},
+                {4, 0},
+                {5, 0},
+                {6, 0},
+                {7, 0},
+                {8, 0}
+            };
+            foreach (var fish in rawFishList)
+            {
+                fishCounts[fish]++;
+            }
 
             for (int day = 0; day < 80; day++)
             {
-                var count = fishList.Count;
-                for (int i = 0; i < count; i++)
-                {
-                    if (fishList[i] == 0)
-                    {
-                        fishList.Add(8);
-                        fishList[i] = 6;
-                    }
-                    else
-                    {
-                        fishList[i]--;
-                    }
-                }
+                var newFish = fishCounts[0];
+                fishCounts[0] = fishCounts[1];
+                fishCounts[1] = fishCounts[2];
+                fishCounts[2] = fishCounts[3];
+                fishCounts[3] = fishCounts[4];
+                fishCounts[4] = fishCounts[5];
+                fishCounts[5] = fishCounts[6];
+                fishCounts[6] = fishCounts[7];
+                fishCounts[7] = fishCounts[8];
+                fishCounts[6] += newFish;
+                fishCounts[8] = newFish;
             }
 
             sw.Stop();
-            Console.WriteLine($"Part 1: {fishList.Count}");
+            Console.WriteLine($"Part 1: {fishCounts.Sum(v => v.Value)}");
             System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
         }
 
@@ -47,27 +61,41 @@ namespace Day06
             var input = File.ReadAllText(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
-            var fishList = input.Split(',').Select(int.Parse).ToList();
+            var rawFishList = input.Split(',').Select(int.Parse);
+            var fishCounts = new Dictionary<int, long>()
+            {
+                {0, 0},
+                {1, 0},
+                {2, 0},
+                {3, 0},
+                {4, 0},
+                {5, 0},
+                {6, 0},
+                {7, 0},
+                {8, 0}
+            };
+            foreach (var fish in rawFishList)
+            {
+                fishCounts[fish]++;
+            }
 
             for (int day = 0; day < 256; day++)
             {
-                var count = fishList.Count;
-                for (int i = 0; i < count; i++)
-                {
-                    if (fishList[i] == 0)
-                    {
-                        fishList.Add(8);
-                        fishList[i] = 6;
-                    }
-                    else
-                    {
-                        fishList[i]--;
-                    }
-                }
+                var newFish = fishCounts[0];
+                fishCounts[0] = fishCounts[1];
+                fishCounts[1] = fishCounts[2];
+                fishCounts[2] = fishCounts[3];
+                fishCounts[3] = fishCounts[4];
+                fishCounts[4] = fishCounts[5];
+                fishCounts[5] = fishCounts[6];
+                fishCounts[6] = fishCounts[7];
+                fishCounts[7] = fishCounts[8];
+                fishCounts[6] += newFish;
+                fishCounts[8] = newFish;
             }
 
-
             sw.Stop();
+            Console.WriteLine($"Part 2: {fishCounts.Sum(v => v.Value)}");
             System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
         }
     }
