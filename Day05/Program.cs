27580d48e139c36a2e162e62c70be3edf20c86a4
Evan C <evanc5@users.noreply.github.com>
2021-12-06 18:55:40 -0500

    start day 5 part 1

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace Day05
 {
@@ -16,8 +18,44 @@ namespace Day05
             var input = File.ReadAllLines(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
+            var matrix = new int[1000, 1000];
+
+            foreach (var line in input)
+            {
+                var split = line.Split(" -> ");
+                var origin = split[0].Split(',').Select(int.Parse).ToArray();
+                var dest = split[1].Split(',').Select(int.Parse).ToArray();
+
+                if (origin[0] == dest[0])
+                {
+                    var min = Math.Min(origin[1], dest[1]);
+                    var max = Math.Max(origin[1], dest[1]);
+                    for (int i = min; i < max; i++)
+                    {
+                        matrix[origin[0], i]++;
+                    }
+                }
+                else if (origin[1] == dest[1])
+                {
+                    var min = Math.Min(origin[0], dest[0]);
+                    var max = Math.Max(origin[0], dest[0]);
+                    for (int i = min; i < max; i++)
+                    {
+                        matrix[i, origin[0]]++;
+                    }
+                }
+                else
+                {
+                }
+            }
 
             sw.Stop();
+            var count = 0;
+            foreach (var point in matrix)
+            {
+                if (point >= 2) count++;
+            }
+            Console.WriteLine($"Part 1: {count}");
             System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
         }
 
