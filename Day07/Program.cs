b7a408492d2e3bd9c5279a91004314a15dc5e1dd
Evan C <evanc5@users.noreply.github.com>
2021-12-07 00:26:44 -0500

    why is this not just the answer though

--- Day07/Program.cs
+++ Day07/Program.cs
@@ -15,17 +15,26 @@ namespace Day07
 
         static void Part1()
         {
-            var input = File.ReadAllLines(@".\input.txt");
+            var rawInput = @"16,1,2,0,4,2,7,1,2,14";
+            var input = rawInput.Split(',');//File.ReadAllText(@".\input.txt").Split(',');
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
+            var positions = input.Select(int.Parse);
+            var average = (int)Math.Round(positions.Average(), 0);
+            var fuel = 0;
+            foreach (var pos in positions)
+            {
+                fuel += Math.Abs(pos - average);
+            }
 
             sw.Stop();
+            Console.WriteLine($"Part 1: {fuel}");
             System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
         }
 
         static void Part2()
         {
-            var input = File.ReadAllLines(@".\input.txt");
+            var input = File.ReadAllText(@".\input.txt").Split(',');
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
 
