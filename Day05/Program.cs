4327effc19aba3532382929614d8befdf189905b
Evan C <evanc5@users.noreply.github.com>
2021-12-06 19:09:43 -0500

    complete part 2

--- Day05/Program.cs
+++ Day05/Program.cs
@@ -30,7 +30,7 @@ namespace Day05
                 {
                     var min = Math.Min(origin[1], dest[1]);
                     var max = Math.Max(origin[1], dest[1]);
-                    for (int i = min; i < max; i++)
+                    for (int i = min; i <= max; i++)
                     {
                         matrix[origin[0], i]++;
                     }
@@ -39,9 +39,9 @@ namespace Day05
                 {
                     var min = Math.Min(origin[0], dest[0]);
                     var max = Math.Max(origin[0], dest[0]);
-                    for (int i = min; i < max; i++)
+                    for (int i = min; i <= max; i++)
                     {
-                        matrix[i, origin[0]]++;
+                        matrix[i, origin[1]]++;
                     }
                 }
                 else
@@ -64,8 +64,54 @@ namespace Day05
             var input = File.ReadAllLines(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
+            var matrix = new int[1000, 1000];
+
+            foreach (var line in input)
+            {
+                var split = line.Split(" -> ");
+                var origin = split[0].Split(',').Select(int.Parse).ToArray();
+                var dest = split[1].Split(',').Select(int.Parse).ToArray();
+
+                if (origin[0] == dest[0])
+                {
+                    var min = Math.Min(origin[1], dest[1]);
+                    var max = Math.Max(origin[1], dest[1]);
+                    for (int i = min; i <= max; i++)
+                    {
+                        matrix[origin[0], i]++;
+                    }
+                }
+                else if (origin[1] == dest[1])
+                {
+                    var min = Math.Min(origin[0], dest[0]);
+                    var max = Math.Max(origin[0], dest[0]);
+                    for (int i = min; i <= max; i++)
+                    {
+                        matrix[i, origin[1]]++;
+                    }
+                }
+                else
+                {
+                    var length = Math.Abs(origin[0] - dest[0]);
+                    var xSlope = (int)Math.CopySign(1, dest[0] - origin[0]);
+                    var ySlope = (int)Math.CopySign(1, dest[1] - origin[1]);
+
+                    for (int i = 0; i <= length; i++)
+                    {
+                        matrix[origin[0], origin[1]]++;
+                        origin[0] += xSlope;
+                        origin[1] += ySlope;
+                    }
+                }
+            }
 
             sw.Stop();
+            var count = 0;
+            foreach (var point in matrix)
+            {
+                if (point >= 2) count++;
+            }
+            Console.WriteLine($"Part 2: {count}");
             System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
         }
     }
