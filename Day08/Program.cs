aeffb4c7cc27fd9b12dfa625930f9dd14444a966
Evan C <evanc5@users.noreply.github.com>
2021-12-08 00:10:51 -0500

    add and configure day 8

---
+++ Day08/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        static void Part1()
+        {
+            var input = File.ReadAllLines(@".\input.txt");
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+
+            sw.Stop();
+            System.Diagnostics.Debug.WriteLine($"Part 1: {sw.Elapsed}");
+        }
+
+        static void Part2()
+        {
+            var input = File.ReadAllLines(@".\input.txt");
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+
+
+            sw.Stop();
+            System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
+        }
+    }
+}
