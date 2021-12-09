fd4ed67c0f2dee3b46c7a36f6999de06ad2a1e57
Evan C <evanc5@users.noreply.github.com>
2021-12-09 13:54:18 -0500

    kind of rework day 8 part 2

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -46,7 +46,6 @@ namespace Day08
             var input = File.ReadAllLines(@".\input.txt");
             var sw = System.Diagnostics.Stopwatch.StartNew();
 
-            var displays = new List<Display>();
             var parsed = input.Select(line => line.Split(" | "));
             foreach (var line in parsed)
             {
@@ -80,44 +79,6 @@ namespace Day08
 
             sw.Stop();
             System.Diagnostics.Debug.WriteLine($"Part 2: {sw.Elapsed}");
-        }
-    }
-
-    public class Display
-    {
-        public bool[] Segments { get; private set; }
-        public string Word { get; private set; }
-
-        public Display(string word)
-        {
-            Word = word;
-            switch (word.Length)
-            {
-                case 2:
-                    //number is 1
-                    Segments = new { false, false, true, false, false, true, false };
-                    break;
-                case 3:
-                    //number is 7
-                    Segments = new { true, false, true, false, false, true, false };
-                    break;
-                case 4:
-                    //number is 4
-                    Segments = new[] { false, true, true, true, false, true, false };
-                    break;
-                case 5:
-                    //number can be 2, 3, 5
-                    Segments = new bool[7];
-                    break;
-                case 6:
-                    //number can be 0, 6, 9
-                    Segments = new bool[7];
-                    break;
-                case 7:
-                    //number is 8
-                    Segments = new[] { true, true, true, true, true, true, true };
-                    break;
-            }
         }
     }
 }
